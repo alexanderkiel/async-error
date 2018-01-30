e988667004c01d8a861f1a25ede0f74210354997
Alexander Kiel <alexanderkiel@gmx.net>
2018-01-30 20:39:20 +0100

    Bump Next Dev Version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.akiel/async-error "0.2"
+(defproject org.clojars.akiel/async-error "0.3-SNAPSHOT"
   :description "Error Handling Utils for core.async."
   :url "https://github.com/alexanderkiel/async-error"
   :license {:name "Eclipse Public License"
